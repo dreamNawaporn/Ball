5d4ceba731b42ffc7eb1b595b8685a3f9495cf37
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-03-06 01:18:29 +0700

    Initial commit

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password ="";
+    $dbname = "footballteam";
+
+//สร้างการเชื่อมต่อ
+    $conn = mysqli_connect($identifier, $first_name, $last_name, $team, $position, $image);
+//check connection
+
+
+
+
+
+
+
+
+?>
