ac8d9895802d4d036b1f8acdd02512c26aa6e8cb
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-03-15 00:27:46 +0700

    2

--- server.php
+++ server.php
@@ -8,7 +8,11 @@
     $conn = mysqli_connect($identifier, $first_name, $last_name, $team, $position, $image);
 //check connection
 
-
+if(!$conn){
+    die("Connection failed". mysqli_connect_error());
+}else{
+    echo "Connected successfull";
+}
 
 
 
